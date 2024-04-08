d3a8be5e5b05bb7c2afa8ba28fa3841b1f6d3376
James Hershey <jameshershey34@gmail.com>
2024-04-08 12:42:22 -0400

    user authentication functioning can create new user, login, get info, and logout

--- Back-end/user_app/serializers.py
+++ Back-end/user_app/serializers.py
@@ -1,5 +1,6 @@
 from .models import User
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 class CreateUser(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +17,25 @@ class CreateUser(serializers.ModelSerializer):
         user.set_password(data['password'])
         user.save()
         return user
+    
+
+class LogIn(serializers.Serializer):
+    username=serializers.CharField()
+    password=serializers.CharField(style={'input_type': 'password'})
+
+    def validate(self, data):
+        username=data.get('username')
+        password=data.get('password')
+
+        if username and password:
+            user=authenticate(request=self.context.get('request'), username=username, password=password)
+
+            if not user:
+                message = 'Incorrect User Credentials.'
+                raise serializers.ValidationError(message, code='authorization')
+        else:
+            message=('Must include username and password')
+            raise serializers.ValidationError(message, code='authorization')
+
+        data['user'] = user
+        return data
--- Back-end/user_app/views.py
+++ Back-end/user_app/views.py
@@ -10,8 +10,10 @@ from rest_framework.status import (
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
+    HTTP_401_UNAUTHORIZED
 )
 from django.contrib.auth import authenticate
+from django.utils import timezone
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from .models import User
@@ -24,18 +26,42 @@ class Sign_up(APIView):
         serializer = CreateUser(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            user.set_password(user.password)
-            user.save()
             token = Token.objects.create(user=user)
             return Response({'user': user.username, "token": token.key}, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
       
 
 class Log_in(APIView):
-    pass
+    def post(self, request):
+        username=request.data.get('username')
+        password=request.data.get('password')
+        user=authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            user.last_login = timezone.now()
+            user.save(update_fields=['last_login'])
+            return Response({'token': token.key, 'user': user.username})
+        else:
+            return Response({"Incorrect login credentials"}, status = HTTP_400_BAD_REQUEST)
 
 class Log_out(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({"message": "User logged out successfully"}, status=HTTP_204_NO_CONTENT)
+
 
 class User_info(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            user_data={
+                "username": user.username,
+                "email": user.email
+            }
+            return Response(user_data, status=HTTP_200_OK)
+        else:
+            return Response({'error': 'User is not authenticated'}, status=HTTP_401_UNAUTHORIZED)
